2f80a725978aafdf074ec0d690a58048c4f19d10
Jain <shubhja@iitk.ac.in>
2016-01-28 21:51:42 +0530

    small tested

--- assign1/meanshift.py
+++ assign1/meanshift.py
@@ -233,5 +233,5 @@ for i in range(height):
 		imgLABComp[i][j][2]=imgLAB[final[i][j][0]][final[i][j][1]][2]
 # cv2.imshow('image',imgLABComp)
 # cv2.waitKey(0)
-# cv2.imwrite(filename[:-4]+'type2'+str(kernel_hs)+str(kernel_hc)+'.png',imgLABComp)
+cv2.imwrite(filename[:-4]+'type2'+str(kernel_hs)+str(kernel_hc)+'.png',imgLABComp)
 # cv2.destroyAllWindows()
