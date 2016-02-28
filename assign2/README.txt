01fc7c145e971b6451d3ed265d9f05407521124a
Jain <shubhja@iitk.ac.in>
2016-02-29 01:55:06 +0530

    assignment 2 updated

---
+++ assign2/README.txt
@@ -0,0 +1,17 @@
+CS676A - Assignment 2
+Feb 10, 2016
+Shubham Jain, 13683
+Vikas Jain, 13788
+----------------------------------------
+Program file is ip.py
+It take 2 command line arguments - name of two image files. Write following command to run the program:
+
+$ python ip.py 'name of file1' 'name of file 2'
+
+e.g.
+
+$ python ip.py 'data/set1/img1.png' 'data/set1/img3.png'
+
+Results images will be stored in the same path as of input files.
+
+Other parameters can be changed, mentioned in the program, by commenting and uncommenting a few lines. 
--- assign2/ip.py
+++ assign2/ip.py
@@ -3,23 +3,25 @@ import cv2
 import numpy as np
 import math
 from sklearn.neighbors import NearestNeighbors
-filename1= sys.argv[1]
-filename2 = sys.argv[2]
-# window = int(sys.argv[4])
-thres_factor  = float(sys.argv[3])
-# windo = int(sys.argv[5])
-windo2 = int(sys.argv[4])
-window=5
-windo=15
-# ssd_thres = 0.97
+filename1= sys.argv[1]            # image 1
+filename2 = sys.argv[2]           # image 2
+# thres_factor  = float(sys.argv[3])      # factor of threshold to be taken; threshold = thres_factor*avg(f)
+thres_factor = 2
+# descriptor_size = int(sys.argv[4])      # size of descriptor window
+descriptor_size = 32
+# H_size = int(sys.argv[5])               # size of window for which H matix to be build
+H_size = 7
+# max_range = int(sys.argv[6])            # range over which f is check for maximum
+max_range = 15
+
 ############### Parameters to tune
 # thres_factor  (parameter)
 # padding
-# H Matrix - window 5
-# lamda- maximum window - windo 15
-# descriptor size - windo2 (parameters)
+# H Matrix - H_size 5
+# lamda- maximum max_range - max_range 15
+# descriptor size - descriptor_size (parameters)
 ##################
-def direction(ix,iy):
+def direction(ix,iy):                # for finding direction of gradient and classify into bins
 	if(ix!=0):
 		m = iy*1.0/ix
 	if (iy>0 and ix>0):
@@ -61,7 +63,7 @@ def ip(filename):
 	direc = [[0 for i in range(max(width,height))] for j in range(max(width,height))]
 	f = [[0 for i in range(max(width,height))] for j in range(max(width,height))]
 	im = gray_img
-	for i in range(height):
+	for i in range(height):                             # Finding gradient for each pixel
 		for j in range(width):
 			if(j == 0):
 				ix[i][j]=int(gray_img[i][1])-int(gray_img[i][0])
@@ -76,52 +78,44 @@ def ip(filename):
 			else:
 				iy[i][j]=(int(gray_img[i+1][j])-int(gray_img[i-1][j]))*1.0/2
 			direc[i][j]=direction(ix[i][j],iy[i][j])
-	# print direc
-	for i in range(height):
+
+	for i in range(height):                              # Calculating H matrix for each pixel
 		for j in range(width):
 			val1=0
 			val2=0
 			val3=0
-			# print i,j
-			for k in range(max(0,i-window),min(height,i+window)):
-				for l in range(max(0,j-window),min(width,j+window)):
+
+			for k in range(max(0,i-H_size),min(height,i+H_size)):
+				for l in range(max(0,j-H_size),min(width,j+H_size)):
 					val1+=ix[k][l]**2
 					val2+=ix[k][l]*iy[k][l]
 					val3+=iy[k][l]**2
-			if(val1+val3==0):
+			if(val1+val3==0):                               # calculating harris descriptor for each pixel using H matrix
 				f[i][j]=0
 			else:
 				f[i][j]=(val1*val3-val2**2)/(val1+val3)
-			f[i][j]/=(window**2)
+			f[i][j]/=(H_size**2)
 
-			# print f[i][j]
 	su = 0
-	for x in range(len(f)):
+	for x in range(len(f)):                                   # setting threshold for harris operator
 		su += sum(f[x])
 	threshold = thres_factor*float(su)/len(f)/len(f[0])
-	for i in range(height):
+	for i in range(height):                                  # selecting points above threshold
 		for j in range(width):
 			if(f[i][j]>threshold):
 				im[i][j]=f[i][j]
 			else:
 				im[i][j]=0
-	# print max(max(f))
-	# data=np.array([f[x/height][x%width] for x in range(height*width)])
-	# hist,bins=np.histogram(data,bins=np.linspace(0,max(max(f)),max(max(f))/20))
-	# print(hist)
 	im2=im
-	# for i in range(1,height-1):
-	# 	for j in range(1,width-1):
-	# 		im[i][j]=(im[i][j-1]+im[i][j+1]+im[i+1][j-1]+im[i+1][j]
-	# +im[i+1][j+1]+im[i-1][j-1]+im[i-1][j+1]+im[i-1][j]+im[i][j])/9
-	points=[]
+
+	points=[]                                     # taking local maxima points
 	for i in range(5,height-5):
 		for j in range(5,width-5):
 			if(im[i][j]==0):
 				continue
 			flag=0
-			for k in range(max(0,i-windo),min(height,i+windo)):
-				for l in range(max(0,j-windo),min(width,j+windo)):
+			for k in range(max(0,i-max_range),min(height,i+max_range)):
+				for l in range(max(0,j-max_range),min(width,j+max_range)):
 					if(im[i][j]<im[k][l]):
 						flag=1
 						break
@@ -129,17 +123,14 @@ def ip(filename):
 				im2[i][j]=0
 			else:
 				points.append([i,j])
-		# 	im2[i][j]=1
-	# print len(points)
 	ip=[]
-	for i in range(len(points)):
-		# print points[i]
+	for i in range(len(points)):                    # creating descriptor for each point
 		desc = []
-		k1 = max(0,points[i][0]-windo2)
-		k2 = min(height,points[i][0]+windo2)
-		l1 = max(0,points[i][1]-windo2)
-		l2 = min(width,points[i][1]+windo2)
-		patchx = np.linspace(k1,k2,5)
+		k1 = max(0,points[i][0]-descriptor_size)
+		k2 = min(height,points[i][0]+descriptor_size)
+		l1 = max(0,points[i][1]-descriptor_size)
+		l2 = min(width,points[i][1]+descriptor_size)
+		patchx = np.linspace(k1,k2,5)               # dividing into 16 patches
 		patchy = np.linspace(l1,l2,5)
 		for k in range(4):
 			for l in range(4):
@@ -150,27 +141,11 @@ def ip(filename):
 							temp[direc[px][py]]=temp[direc[px][py]]+1
 				for hist in temp:
 					desc.append(hist)
-		# print desc
-		# for k in range(k1, k1+patch)
-		# for k in range(max(0,points[i][0]-windo2),min(height,points[i][0]+windo2)):
-		# 	for l in range(max(0,points[i][1]-windo2),min(width,points[i][1]+windo2)):
-		# 		if(direc[k][l]!=-1):
-		# 			temp[direc[k][l]]=temp[direc[k][l]]+1
-		# for j in range(len(temp)):
-		# 	temp[j]=temp[j]*1.0/(windo2**2)/4
-		# print temp
 		ip.append([desc,points[i]])
-	return [img,im2,ip]
+	return [img,im2,ip]               # return original image, interest point image and list of interest points
 
-
-# def SSD(hog1, hog2):
-# 	ssd = 0
-# 	for i in range(len(hog1)):
-# 		ssd += (hog1[i] - hog2[i])**2
-# 	return ssd
-
-def intersection(ip1, ip2):
-	neigh = NearestNeighbors(n_neighbors = 2, metric = 'l2')
+def intersection(ip1, ip2):                            # finding match of interest points for 2 images
+	neigh = NearestNeighbors(n_neighbors = 2, metric = 'l2')              # using KNN
 	tr_data = []
 	tr_points = []
 	for i in ip2:
@@ -184,63 +159,38 @@ def intersection(ip1, ip2):
 	neigh.fit(tr_data)
 	results = neigh.kneighbors(ts_data, 2, return_distance = True)
 	match = []
-	no = 0
-	notot = 0
 	for i in range(len(results[0])):
-		notot += 1
-		# if (results[0][i][0]/results[0][i][1] <= ssd_thres ):
 		IP2 = tr_points[results[1][i][0]]
 		IP1 = ts_points[i]
 		match.append([IP1, IP2, results[0][i][0]/results[0][i][1]])
-		no += 1
-	print notot
-	print no
-	return match
-# def intersection(ip1, ip2):
-# 	match = []
-# 	no_ip1 = 0
-# 	for i in ip1:
-# 		fl=0
-# 		lssd = 1280000
-# 		no_ip2 = 0
-# 		for j in ip2:
-# 			ssd = SSD(i[0],j[0])
-# 			if (ssd < lssd):
-# 				lssd = ssd
-# 				temp=j
-# 				fl=1
-# 			no_ip2 += 1
-# 		no_ip1 += 1
-# 		if(fl==1):
-# 			match.append([i[1],temp[1],lssd])
-# 	return match
+	return match                            # return list of matches along with corresponding ratio of best match to second best match
+
+# MAIN PROGRAM
+[img1,ip1image,ip1]=ip(filename1)          # find interest points of image1
+[img2,ip2image,ip2]=ip(filename2)          # find interest points of image 2
+
+matches = intersection(ip1,ip2)            # find matches between interest points of two images
+matches = sorted(matches,key=lambda matches: matches[2])              # sort matches using ratio of best match to second best match
 
 
-# cv2.imshow('image',im)
-[img1,ip1image,ip1]=ip(filename1)
-[img2,ip2image,ip2]=ip(filename2)
-# intersection(ip1,ip2)
-matches = intersection(ip1,ip2)
-matches = sorted(matches,key=lambda matches: matches[2])
-# for x in matches:
-# 	print x
-# print match
+# Creating combined image to visualize
 h1, w1 = img1.shape[:2]
 h2, w2 = img2.shape[:2]
-# print h1,w1,h2,w2
+
 nWidth = w1+w2
 nHeight = max(h1, h2)
 hdif = (h1-h2)/2
 newimg = np.zeros((nHeight, nWidth, 3), np.uint8)
 newimg[hdif:hdif+h2, :w2] = img2
 newimg[:h1, w2:w1+w2] = img1
-for i in range(min(len(matches),50)):
+for i in range(min(len(matches),50)):                # select to 50 best match points
 	pt_a = (int(matches[i][1][1]), int(matches[i][1][0]+hdif))
 	pt_b = (int(matches[i][0][1]+w2), int(matches[i][0][0]))
 	cv2.line(newimg, pt_a, pt_b, (0, 255, 0),1)
 	cv2.circle(newimg,(matches[i][1][1],matches[i][1][0]+hdif), 3 , (0,0,255), -1)
 	cv2.circle(newimg,(matches[i][0][1]+w2,matches[i][0][0]), 3, (0,0,255), -1)
-cv2.imwrite(filename1[:-4]+str(thres_factor)+str(window)+str(windo)+str(windo2)+'combine.png',newimg)
-cv2.imwrite(filename1[:-4]+str(thres_factor)+str(window)+str(windo)+str(windo2)+'lam.png',ip1image)
-cv2.imwrite(filename2[:-4]+str(thres_factor)+str(window)+str(windo)+str(windo2)+'lam.png',ip2image)
-# cv2.waitKey(0)
+
+# saving results
+cv2.imwrite(filename1[:-4]+str(thres_factor)+str(H_size)+str(max_range)+str(descriptor_size)+'combine.png',newimg)  # combined image with matches
+cv2.imwrite(filename1[:-4]+str(thres_factor)+str(H_size)+str(max_range)+str(descriptor_size)+'interestpoints.png',ip1image)  # interest points of image 1
+cv2.imwrite(filename2[:-4]+str(thres_factor)+str(H_size)+str(max_range)+str(descriptor_size)+'interestpoints.png',ip2image)  # interest points of image 2
